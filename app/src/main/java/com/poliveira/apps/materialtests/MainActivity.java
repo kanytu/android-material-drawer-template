2f680b765de470aa7c8f83d65923708ffae67672
Pedro Oliveira <pedro.oliveira@janeladigital.com>
2014-12-11 11:36:05 +0000

    Fixed an issue that would cause the system bar to not have colors

--- app/src/main/java/com/poliveira/apps/materialtests/MainActivity.java
+++ app/src/main/java/com/poliveira/apps/materialtests/MainActivity.java
@@ -16,11 +16,10 @@ public class MainActivity extends ActionBarActivity implements NavigationDrawerC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_topdrawer);
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
         mNavigationDrawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
         mNavigationDrawerFragment.setup(R.id.fragment_drawer, (DrawerLayout) findViewById(R.id.drawer), mToolbar);
     }
