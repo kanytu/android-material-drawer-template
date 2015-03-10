8b46066493d4f3711e299c0e5e4cc0ebc2cde545
swolford <swolford@users.noreply.github.com>
2015-03-10 11:17:54 +0100

    Added closing soft keyboard on drawer events
    Close the soft keyboard on drawer open/close events. Tested on Android 4.0.4 and 5.0.2

--- app/src/main/java/com/poliveira/apps/materialtests/NavigationDrawerFragment.java
+++ app/src/main/java/com/poliveira/apps/materialtests/NavigationDrawerFragment.java
@@ -15,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -91,6 +92,9 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
         mActionBarDrawerToggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
             @Override
             public void onDrawerClosed(View drawerView) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                
                 super.onDrawerClosed(drawerView);
                 if (!isAdded()) return;
                 getActivity().invalidateOptionsMenu();
@@ -98,6 +102,9 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
 
             @Override
             public void onDrawerOpened(View drawerView) {
+                InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                
                 super.onDrawerOpened(drawerView);
                 if (!isAdded()) return;
                 if (!mUserLearnedDrawer) {
